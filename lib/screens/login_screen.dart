c1bb5d56053b139976bc3c6cd8355d8249b1542b
Sathsara <chalithsathsara@gmail.com>
2025-06-25 21:08:16 +0530

    implement login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,329 @@
+import 'package:flutter/material.dart';
+
+class Logingscreen extends StatefulWidget {
+  const Logingscreen({super.key});
+
+  @override
+  State<Logingscreen> createState() => _LogingscreenState();
+}
+
+class _LogingscreenState extends State<Logingscreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+  bool _isPasswordVisible = false;
+
+  Widget _buildLogo() {
+    return Image.asset(
+      'assets/images/logo.png', // your logo image file path
+      height: 180, // set your desired height
+      fit: BoxFit.contain, // optional: keep aspect ratio
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      style: const TextStyle(fontSize: 16), // input text size
+      decoration: InputDecoration(
+        hintText: 'Email or username',
+        hintStyle: const TextStyle(
+          color: Color(0xFFFF6600),
+          fontSize: 14, // standard hint size
+        ),
+        prefixIcon: const Icon(Icons.person_outline, size: 20),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 12,
+        ),
+        filled: true,
+        fillColor: const Color(0xFFFFE5D0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      onSaved: (value) {
+        _email = value!;
+      },
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      style: const TextStyle(fontSize: 16),
+      obscureText: !_isPasswordVisible,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        hintStyle: const TextStyle(color: Color(0xFFFF6600), fontSize: 14),
+        prefixIcon: const Icon(Icons.lock_outline, size: 20),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility_off : Icons.visibility,
+            size: 20,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 12,
+        ),
+        filled: true,
+        fillColor: const Color(0xFFFFE5D0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      onSaved: (value) {
+        _password = value!;
+      },
+    );
+  }
+
+  Widget _buildForgotPassword() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Forget Password?',
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 13,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFFE65100), // Deep orange (start)
+            Color(0xFFFF6D00), // Bright orange (light)
+            Color(0xFFD65A00), // Medium dark orange (middle - subtle darker)
+            Color(0xFFFF6D00), // Bright orange (light)
+            Color(0xFFE65100), // Deep orange (end)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.0, 0.25, 0.5, 0.75, 1.0],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 5, 5, 5).withOpacity(0.4),
+            spreadRadius: 1,
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor:
+                Colors.transparent, // make button background transparent
+            shadowColor: Colors
+                .transparent, // remove button shadow, using container shadow
+            padding: const EdgeInsets.symmetric(vertical: 14),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 0,
+          ),
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              print('Email: $_email');
+              print('Password: $_password');
+            }
+          },
+          child: const Text(
+            'Log In',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold, // white text on gradient background
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrDivider() {
+    return Row(
+      children: const [
+        Expanded(child: Divider(color: Colors.red)),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text("OR", style: TextStyle(color: Colors.red, fontSize: 13)),
+        ),
+        Expanded(child: Divider(color: Colors.red)),
+      ],
+    );
+  }
+
+  Widget _buildGoogleButton() {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 6,
+            offset: const Offset(0, 4), // shadow direction: bottom right
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SizedBox(
+        width: double.infinity, // full width
+        child: OutlinedButton.icon(
+          style: OutlinedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 14),
+            backgroundColor: Colors.white,
+            side: const BorderSide(
+              color: Colors.transparent,
+            ), // no outline border
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          icon: Image.asset('assets/images/google.png', height: 20),
+          label: const Text(
+            'Continue with Google',
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            print("Google login clicked");
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRegisterText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('No account?', style: TextStyle(fontSize: 13)),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            'Register',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 13,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // Use a Container to apply gradient background
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFBD2D01),
+              Color(0xFFCF4602),
+              Color(0xFFF67F00),
+              Color(0xFFCF4602),
+              Color(0xFFBD2D01),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.0, 0.25, 0.5, 0.75, 1.0],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20), // reduced top spacing
+                  _buildLogo(),
+                  const SizedBox(height: 15), // reduced spacing
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(
+                            255,
+                            5,
+                            5,
+                            5,
+                          ).withOpacity(0.4),
+                          spreadRadius: 1,
+                          blurRadius: 8,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 12,
+                    ), // reduced vertical padding
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.deepOrange,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          _buildEmailField(),
+                          const SizedBox(height: 12),
+                          _buildPasswordField(),
+                          _buildForgotPassword(),
+                          const SizedBox(height: 8),
+                          _buildLoginButton(),
+                          const SizedBox(height: 12),
+                          _buildOrDivider(),
+                          const SizedBox(height: 8),
+                          _buildGoogleButton(),
+                          const SizedBox(height: 8),
+                          _buildRegisterText(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20), // reduced bottom spacing
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
